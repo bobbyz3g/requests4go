01756dfbdbb32679a6ed406406863cf3b9cdd544
Zhang liangliang <zhangliangliang1996@hotmail.com>
2019-11-24 15:48:05 +0800

    :page_facing_up: Add Copytright comments for code files.

--- session_test.go
+++ session_test.go
@@ -1,3 +1,7 @@
+// Copyright 2019 The Kaiser925. All rights reserved.
+// Use of this source code is governed by a Apache
+// license that can be found in the LICENSE file.
+
 package requests4go
 
 import "testing"
