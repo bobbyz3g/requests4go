1b43e3d28d2c1c94ca17af41cfe06074f61c1e6d
Lucas.zhang <zhangliang925@icloud.com>
2021-01-25 22:50:15 +0800

    :sparkles: Add RequestOption
    - Add NewRequestWithOpt and NewRequestWithOptCtx
    - Add Params and Auth option

---
+++ requestopt.go
@@ -0,0 +1,67 @@
+// Developed by Kaiser925 on 2021/1/25.
+// Lasted modified 2021/1/25.
+// Copyright (c) 2021.  All rights reserved
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//     http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package requests4go
+
+import (
+	"context"
+	"net/http"
+)
+
+// A RequestOption is represent a option of request.
+type RequestOption func(req *http.Request)
+
+// NewRequestWithOpt wrapper the NewRequestWithOptCtx
+func NewRequestWithOpt(
+	method string,
+	url string,
+	opts ...RequestOption,
+) (*http.Request, error) {
+	return NewRequestWithOptCtx(context.Background(), method, url, opts...)
+}
+
+// NewRequestWithOptCtx build a new *http.Request with Context and RequestOption
+func NewRequestWithOptCtx(
+	ctx context.Context,
+	method string,
+	url string,
+	opts ...RequestOption,
+) (*http.Request, error) {
+
+	req, err := http.NewRequestWithContext(ctx, method, url, nil)
+	if err != nil {
+		return nil, err
+	}
+	for _, opt := range opts {
+		opt(req)
+	}
+	return req, nil
+}
+
+// Params set url query parameters for the request.
+func Params(params map[string]string) RequestOption {
+	return func(req *http.Request) {
+		q := req.URL.Query()
+		for key, value := range params {
+			q.Set(key, value)
+		}
+		req.URL.RawQuery = q.Encode()
+	}
+}
+
+// Auth set basic auth for the request.
+func Auth(name string, password string) RequestOption {
+	return func(req *http.Request) {
+		req.SetBasicAuth(name, password)
+	}
+}
---
+++ requestopt_test.go
@@ -0,0 +1,62 @@
+// Developed by Kaiser925 on 2021/1/25.
+// Lasted modified 2021/1/25.
+// Copyright (c) 2021.  All rights reserved
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//     http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package requests4go
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestParams(t *testing.T) {
+	params := map[string]string{
+		"b": "b",
+		"a": "c",
+	}
+	req, _ := NewRequestWithOpt("get", "http://simple.org/path/?b=a", Params(params))
+	assert.Equal(t, req.URL.String(), "http://simple.org/path/?a=c&b=b")
+}
+
+var authTests = []struct {
+	username, password string
+	ok                 bool
+}{
+	{"Aladdin", "open sesame", true},
+	{"Aladdin", "open:sesame", true},
+	{"", "", true},
+}
+
+func TestAuth(t *testing.T) {
+	for _, tt := range authTests {
+		r, _ := NewRequestWithOpt("GET", "http://example.com/", Auth(tt.username, tt.password))
+		username, password, ok := r.BasicAuth()
+		assert.Equal(t, ok, tt.ok)
+		assert.Equal(t, username, tt.username)
+		assert.Equal(t, password, tt.password)
+	}
+}
+
+func TestAll(t *testing.T) {
+	params := map[string]string{
+		"b": "b",
+		"a": "c",
+	}
+	req, _ := NewRequestWithOpt("get", "http://simple.org/path/?b=a",
+		Params(params),
+		Auth(authTests[0].username, authTests[0].password))
+	assert.Equal(t, req.URL.String(), "http://simple.org/path/?a=c&b=b")
+	username, password, ok := req.BasicAuth()
+	assert.Equal(t, ok, authTests[0].ok)
+	assert.Equal(t, username, authTests[0].username)
+	assert.Equal(t, password, authTests[0].password)
+}
