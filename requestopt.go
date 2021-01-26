a78b9158db7534eb24b3e76dc1b8e4001029785f
Lucas.zhang <zhangliang925@icloud.com>
2021-01-26 18:25:46 +0800

    :sparkles: Return error for RequestOption

--- requestopt.go
+++ requestopt.go
@@ -19,7 +19,7 @@ import (
 )
 
 // A RequestOption is represent a option of request.
-type RequestOption func(req *http.Request)
+type RequestOption func(req *http.Request) error
 
 // NewRequestWithOpt wrapper the NewRequestWithOptCtx
 func NewRequestWithOpt(
@@ -37,31 +37,34 @@ func NewRequestWithOptCtx(
 	url string,
 	opts ...RequestOption,
 ) (*http.Request, error) {
-
 	req, err := http.NewRequestWithContext(ctx, method, url, nil)
 	if err != nil {
 		return nil, err
 	}
 	for _, opt := range opts {
-		opt(req)
+		if err := opt(req); err != nil {
+			return nil, err
+		}
 	}
 	return req, nil
 }
 
 // Params set url query parameters for the request.
 func Params(params map[string]string) RequestOption {
-	return func(req *http.Request) {
+	return func(req *http.Request) error {
 		q := req.URL.Query()
 		for key, value := range params {
 			q.Set(key, value)
 		}
 		req.URL.RawQuery = q.Encode()
+		return nil
 	}
 }
 
 // Auth set basic auth for the request.
 func Auth(name string, password string) RequestOption {
-	return func(req *http.Request) {
+	return func(req *http.Request) error {
 		req.SetBasicAuth(name, password)
+		return nil
 	}
 }
